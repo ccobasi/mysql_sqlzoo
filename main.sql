a29c3d75ed409690227a751d9d4868da20c11c8d
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-03-05 19:50:49 +0100

    (Add)join to main.sql

--- main.sql
+++ main.sql
@@ -211,3 +211,56 @@ SELECT continent
 FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000;
+
+-- join
+SELECT matchid, player
+FROM goal 
+WHERE teamid = 'GER';
+
+SELECT id,stadium,team1,team2
+FROM game
+WHERE id = 1012;
+
+SELECT player,teamid, stadium, mdate
+FROM goal JOIN game ON (goal.matchid = game.id)
+WHERE teamid = 'GER';
+
+SELECT team1, team2, player
+FROM game JOIN goal ON (game.id = goal.matchid)
+WHERE player LIKE 'Mario%';
+
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON (goal.teamid = eteam.id)
+WHERE gtime<=10;
+
+SELECT mdate, teamname
+FROM game JOIN eteam ON (game.team1 = eteam.id)
+WHERE coach = 'Fernando Santos';
+
+SELECT player
+FROM goal JOIN game ON (goal.matchid = game.id)
+WHERE stadium = 'National Stadium, Warsaw';
+
+SELECT DISTINCT(player)
+FROM goal JOIN game ON (goal.matchid = game.id) 
+WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid != 'GER';
+
+SELECT teamname, COUNT(*)
+FROM eteam JOIN goal ON id=teamid
+GROUP BY teamname
+ORDER BY teamname;
+
+SELECT stadium, COUNT(*)
+FROM game JOIN goal ON (game.id = goal.matchid)
+GROUP BY stadium
+ORDER BY stadium;
+
+SELECT matchid, mdate, COUNT(Player)
+FROM game JOIN goal ON matchid = id 
+WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid, mdate;
+
+SELECT matchid, mdate, COUNT(player)
+FROM game JOIN goal ON (game.id = goal.matchid)
+WHERE teamid = 'GER'
+GROUP BY matchid, mdate;
