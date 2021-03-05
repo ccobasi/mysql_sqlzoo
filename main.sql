0996fcaee3844f300360b78a2833dfe93c1108fc
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-03-05 19:48:20 +0100

    (Add)sum and count to main.sql

--- main.sql
+++ main.sql
@@ -179,4 +179,35 @@ ALL(SELECT gdp
 FROM world
 WHERE continent = 'Europe' AND GDP > 0);
 
+-- sum and count
+SELECT SUM(population)
+FROM world;
 
+SELECT DISTINCT(continent)
+FROM world;
+
+SELECT SUM(GDP)
+FROM world
+WHERE continent = 'Africa';
+
+SELECT COUNT(name)
+FROM world
+WHERE area >=1000000;
+
+SELECT SUM(population)
+FROM world
+WHERE name = 'Estonia' OR name = 'Latvia' OR name = 'Lithuania';
+
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
+
+SELECT continent, COUNT(name)
+FROM world
+WHERE population >= 10000000
+GROUP BY continent;
+
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
