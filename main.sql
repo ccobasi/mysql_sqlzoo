4d636a58a2b8edfe625c2080a5bd0d210547292d
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-03-05 18:43:00 +0100

    (Add)select from nobel to main.sql

--- main.sql
+++ main.sql
@@ -66,3 +66,72 @@ SELECT name
 FROM world
 WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%'
 AND name NOT LIKE '% %';
+
+--select from nobel
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1950
+
+SELECT winner
+FROM nobel
+WHERE yr = 1962
+AND subject = 'Literature'
+
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
+
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000;
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989;
+
+SELECT * 
+FROM nobel
+WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama')
+
+SELECT winner 
+FROM nobel
+WHERE winner LIKE 'John%';
+
+SELECT yr, subject, winner 
+FROM nobel
+WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984;
+
+SELECT yr, subject, winner 
+FROM nobel
+WHERE yr = 1980 AND subject != 'Chemistry' AND subject != 'Medicine';
+
+SELECT yr, subject, winner 
+FROM nobel
+WHERE yr < 1910 AND subject = 'Medicine' OR yr >= 2004 AND subject = 'Literature';
+
+SELECT *
+FROM nobel
+WHERE winner = 'PETER GRÜNBERG'; 
+
+SELECT *
+FROM nobel
+WHERE winner = 'EUGENE O''NEILL';
+
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner;
+
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY 
+CASE
+WHEN subject IN ('Physics', 'Chemistry') THEN 1
+ELSE 0
+END,
+subject, winner;
